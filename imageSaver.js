bec9b6537922fc03756b20e70c2d3191abea9a6c
hasbisevinc <flashasbi@gmail.com>
2018-06-15 16:57:47 +0300

    image uploader

--- imageSaver.js
+++ imageSaver.js
@@ -1,3 +1,19 @@
+var uploadImageToBrowser = function(e) {
+    var reader = new FileReader();
+    reader.onload = function(event){
+        var img = new Image();
+        img.onload = function(){
+            userSS.src = img.src;
+        }
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);     
+}
+
+var imageLoader = document.getElementById('uploadImage');
+var userSS = document.getElementById('userSS');
+imageLoader.addEventListener('change', uploadImageToBrowser, false);
+
 var saveImage = function() {
     html2canvas(document.querySelector("#mainContainer")).then(canvas => {
         Canvas2Image.saveAsPNG(canvas, 1280, 720, "png");
